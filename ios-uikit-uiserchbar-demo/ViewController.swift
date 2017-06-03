a72d64c05ab641c4d2065d6b8febe36e81062661
y.okudera <mail@sample.com>
2017-06-03 11:11:10 +0900

    [Add]  キャンセルボタン押下時に呼ばれるデリゲートメソッドを追加

--- ios-uikit-uiserchbar-demo/ViewController.swift
+++ ios-uikit-uiserchbar-demo/ViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    // MARK: - view life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
 
+    // MARK: - private methods
+    
     private func setup() {
         setupDelegate()
         setupShowCancelButton()
@@ -50,17 +54,27 @@ class ViewController: UIViewController {
     }
 }
 
-///MARK: - UISearchBarDelegate
+// MARK: - UISearchBarDelegate
 extension ViewController: UISearchBarDelegate {
     
     /// 検索をクリックしたときに呼ばれる
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print(#function)
+        if let searchText = searchBar.text {
+            print("searchText:\(searchText)")
+        }
     }
     
     /// サーチバーの中身が更新されるときに呼ばれる
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(#function)
         print(searchText)
+    }
+    
+    /// キャンセルボタンをクリックしたときに呼ばれる
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
     }
 }
 
